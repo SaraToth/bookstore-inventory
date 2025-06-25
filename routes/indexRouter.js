274fc8032fe486dd220be27ebf21f0542fb04b98
SaraToth <saratoth101@gmail.com>
2025-06-25 22:06:27 +0900

    Style: Remove unneeded comments

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,10 +5,3 @@ const { getIndex } = require("../controllers/indexController");
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
-
-
-// Nav bar:
-// Home     Books         Branches
-
-// Views: Home
-//Partial views: navbar
