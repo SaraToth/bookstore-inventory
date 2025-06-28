8642f1cd4a7bd2146970413ed07c62971df553dc
SaraToth <saratoth101@gmail.com>
2025-06-28 23:23:09 +0900

    Build: Finish setting up for production

--- db/pool.js
+++ db/pool.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const localDatabaseURL = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
+const isProduction = !!process.env.DATABASE_URL;
+
 module.exports = new Pool({
     connectionString: process.env.DATABASE_URL || localDatabaseURL,
+    ssl: isProduction ? { rejectUnauthorized: false} : false,
 });
